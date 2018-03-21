94330ed3d85fed3adaeeaa0ba533f8de91435393
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-03-21 16:32:32 +0100

    Add netex java model as dependency. Start mapping to netex

--- src/main/java/org/entur/netex/conversion/osm/OsmToNetexTariffZone.java
+++ src/main/java/org/entur/netex/conversion/osm/OsmToNetexTariffZone.java
@@ -17,29 +17,37 @@ package org.entur.netex.conversion.osm;
 
 import org.openstreetmap.osm.Node;
 import org.openstreetmap.osm.Osm;
+import org.openstreetmap.osm.Tag;
+import org.rutebanken.netex.model.*;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.stream.StreamSource;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.math.BigInteger;
+import java.time.OffsetDateTime;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OsmToNetexTariffZone {
 
 
-
-
-    public static void main(String[] args) throws FileNotFoundException, XMLStreamException, JAXBException {
+    public static void main(String[] args) throws FileNotFoundException, JAXBException {
 
         String file = args.length == 0 ? "osm.xml" : args[0];
 
         System.out.println("File arg = " + file);
 
+
+        OsmToNetexTariffZone osmToNetexTariffZone = new OsmToNetexTariffZone();
+        osmToNetexTariffZone.transform(file);
+    }
+
+    private void transform(String file) throws JAXBException, FileNotFoundException {
 
         JAXBContext osmContext = JAXBContext.newInstance(Osm.class);
 
@@ -52,18 +60,61 @@ public class OsmToNetexTariffZone {
 
         Map<BigInteger, Node> nodeMap = osm.getNode().stream()
                 .collect(Collectors.toMap(Node::getId
-                , node -> node));
+                        , node -> node));
 
-        System.out.println("Read "+ nodeMap.size() + "nodes");
+        System.out.println("Read " + nodeMap.size() + "nodes");
+
+        SiteFrame siteFrame = new SiteFrame();
+        siteFrame.withTariffZones(new TariffZonesInFrame_RelStructure());
 
         osm.getWay().forEach(way -> {
             System.out.println("About to read way: " + way.getId());
-            System.out.println(way);
-            System.out.println(way.getId());
+
+            TariffZone tariffZone = new TariffZone();
+
+            String tariffZoneCodeSpace = null;
+            String tariffZoneReference = null;
+
+            for (Tag tag : way.getTag()) {
+
+                if (tag.getK().equals("tariffZoneCodespace")) {
+                    tariffZoneCodeSpace = tag.getV();
+                } else if (tag.getK().startsWith("tariffZoneName")) {
+                    String keyName = tag.getK();
+                    String lang = keyName.substring(keyName.lastIndexOf(':') + 1);
+                    tariffZone.setName(new MultilingualString().withValue(tag.getV()).withLang(lang));
+                } else if (tag.getK().startsWith("tariffZoneReference")) {
+                    tariffZoneReference = tag.getV();
+                }
+            }
+
+            tagValueNotNullOrExit("tariffZoneCodeSpace", tariffZoneCodeSpace);
+            tagValueNotNullOrExit("tariffZoneReference", tariffZoneReference);
+
+            String id = tariffZoneCodeSpace + ":TariffZone:" + tariffZoneReference;
+
+            System.out.println("created id: " + id);
+
+            System.out.println(tariffZone);
+            siteFrame.getTariffZones().getTariffZone().add(tariffZone);
         });
 
 
+        ObjectFactory netexObjectFactory = new ObjectFactory();
+        PublicationDeliveryStructure publicationDeliveryStructure = new PublicationDeliveryStructure()
+                .withPublicationTimestamp(OffsetDateTime.now())
+                .withDataObjects(new PublicationDeliveryStructure.DataObjects()
+                        .withCompositeFrameOrCommonFrame(netexObjectFactory.createSiteFrame(siteFrame)));
 
+
+    }
+
+    private void tagValueNotNullOrExit(String name, String value) {
+
+        if (value == null) {
+            System.err.println(name + " from tag was null");
+            System.exit(1);
+        }
     }
 
 
